43fc5af49c524b49b5ce43b5b128dd96e18ad1d1
Yashar Najafi <yasharnajafi2028@gmail.com>
2026-02-16 13:40:05 +0100

    Manually ran the formatting with pre-commit run --all-files and sync the formatting with lint.yml workflow

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,7 +1,6 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from app.core.config import settings
-
 
 engine = create_async_engine(
     settings.DATABASE_URL,
--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -1,12 +1,11 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
 
 from app.core.config import settings
 from app.db.base import Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -74,9 +73,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
